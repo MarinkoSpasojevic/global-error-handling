beffa4039b59b3f3c588215e673ec91282ed540a
Marinko <marinkospasojevic@gmail.com>
2018-06-25 15:43:51 +0200

    Starting project prepared

--- Global-Error-Handling-Start/GlobalErrorHandling/GlobalErrorHandling/Models/Student.cs
+++ Global-Error-Handling-Start/GlobalErrorHandling/GlobalErrorHandling/Models/Student.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApplication1.Models
+namespace GlobalErrorHandling.Models
 {
     public class Student
     {
